5a17b833a010b35a1746dd5bcc8522184b3a7ae5
jimc27516 <jim@smallcampbell.com>
2025-02-02 20:50:19 -0500

    converted to unittest framework

--- functional_tests.py
+++ functional_tests.py
@@ -1,27 +1,33 @@
+import unittest
 from selenium import webdriver
 
-# browser = webdriver.Safari()
-# browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Safari()
+        return super().setUp()
+    
+    def tearDown(self):
+        self.browser.quit()
+        return super().tearDown()
 
-# assert "Congratulations!" in browser.title
-# print("OK")
+    def test_can_start_a_todo_list(self):
+        # the user navigates to the url
+        self.browser.get("http://localhost:8000")
 
-browser = webdriver.Safari()
+        # the welcome page has "To-Do" in the title
+        self.assertIn("To-Do", self.browser.title)
 
-# the user navigates to the url
-browser.get("http://localhost:8000")
+        self.fail("Finish the test!")
 
-# the welcome page has "To-Do" in the title
-assert "To-Do" in browser.title
+        # the page invites the user to enter a todo item straight away, they just start typing
 
-# the page invites the user to enter a todo item straight away, they just start typing
+        # the user types "buy peacock feathers" and hits enter
 
-# the user types "buy peacock feathers" and hits enter
+        # the page refreshes and now shows "1: Buy peacock feathers" as an item in a to-do list
 
-# the page refreshes and now shows "1: Buy peacock feathers" as an item in a to-do list
+        # the user types "use feathers to make a fly", hits enter
 
-# the user types "use feathers to make a fly", hits enter
+        # the page refreshes and now shows a second item labeled "2: use feathers to make a fly" as an item
 
-# the page refreshes and now shows a second item labeled "2: use feathers to make a fly" as an item
-
-browser.quit()
+if __name__ == "__main__":
+    unittest.main()
