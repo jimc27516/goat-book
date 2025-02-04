43c8b226e2bd71b42f3f00dc57b6be6d3aaf61d1
jimc27516 <jim@smallcampbell.com>
2025-02-03 19:36:28 -0500

    url config, map / to home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+  
+
+    def test_home_page_returns_correct_html_2(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
